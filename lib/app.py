0494c3c7977f2cbbaa51e0fddcd0d7d0dd77d013
SultanTipa1 <josephngumbaugush@gmail.com>
2024-08-27 12:56:02 +0300

    Adds all files

---
+++ lib/app.py
@@ -0,0 +1,9 @@
+#print("Hello World!")
+#print("Hello sun!")
+#print("Hello sky!")
+#print("Helo world!", end=" ")
+#print("Hello sun!", end="!! ")
+#print("Hello sky!", end="!!!\n") # \n - means new line
+
+#print("Hello World!", "Hello sky!\n")
+print("Hello World! Pass this test, please.")
